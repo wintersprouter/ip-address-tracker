94ab9fede5234c4941d23f5073a3580ff1f5aff4
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-09-19 22:01:08 +0800

    feat: delete defult view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    IP Address Tracker Search for any IP address or domain IP Address Location
+    Timezone ISP
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
