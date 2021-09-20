1a58035a8494ea50f2298c03102b9dbafa7ece12
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-09-20 16:15:20 +0800

    style: modify search bar desktop version

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="search-bar w-full flex">
+  <div class="search-bar w-full sm:w-2/5 flex sm:mx-auto">
     <form class="w-full">
       <input
         type="text"
         class="
           h-16
           w-10/12
+          xl:w-9/10
           text-gary-deufult text-md
-          placeholder-gray-500 placeholder-opacity-75
+          placeholder-gray placeholder-opacity-75
           rounded-l-xl
           py-1
           px-2
+          lg:px-5
         "
         placeholder="Search for any IP address or domain"
       />
-      <button class="h-16 bg-black rounded-r-xl w-2/12 py-1" type="submit">
+      <button
+        class="h-16 bg-black rounded-r-xl w-2/12 xl:w-1/10 py-1"
+        type="submit"
+      >
         <span class="text-white font-bold btn-icon">></span>
       </button>
     </form>
