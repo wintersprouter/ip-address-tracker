07299fe3b5381db9b021aebe66f3f93730469296
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-09-20 18:48:33 +0800

    style: modify result card desktop version

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -2,17 +2,25 @@
   <div
     class="
       w-full
-      sm:w-2/5
+      border-2
       sm:mx-auto
       rounded-xl
       bg-white
       mt-6
       text-center
+      sm:text-left
       pt-3
       pb-6
+      sm:flex
+      sm:py-6
+      sm:mt-28
+      sm:w-8/10
+      xl:w-4/6
+      sm:justify-around
+      sm:divide-x-2 sm:divide-opacity-10 sm:divide-gray-light
     "
   >
-    <div class="mt-3" v-for="item in items" :key="item.id">
+    <div class="mt-3 sm:mt-0 sm:px-4" v-for="item in items" :key="item.id">
       <div class="text-gray uppercase font-medium tracking-wider">
         {{ item.title }}
       </div>
