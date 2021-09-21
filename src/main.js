af48beef0a0f5b9faed095470fbb35e056dab1fa
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-09-21 19:29:44 +0800

    feat: build a map and show current user's location

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
+import { Icon } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import Vue from 'vue'
+import { LIcon, LMap, LMarker, LPopup, LTileLayer } from 'vue2-leaflet'
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
+Vue.component('l-map', LMap)
+Vue.component('l-tile-layer', LTileLayer)
+Vue.component('l-marker', LMarker)
+Vue.component('l-popup', LPopup)
+Vue.component('l-icon', LIcon)
+delete Icon.Default.prototype._getIconUrl
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 Vue.config.productionTip = false
 
