99a77eb133a6a8767c84bf6344054b76624234a7
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-09-22 14:59:57 +0800

    feat: add input vaildation

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="search-bar w-full sm:w-2/5 flex sm:mx-auto">
-    <form class="w-full">
+    <form class="w-full" @submit.stop.prevent="handleSubmit">
       <input
         type="text"
+        v-model="ip"
         class="
           h-16
           w-10/12
@@ -14,6 +15,7 @@
           px-2
           lg:px-5
         "
+        maxlength="20"
         placeholder="Search for any IP address."
       />
       <button
@@ -27,7 +29,43 @@
 </template>
 
 <script>
+import { Toast } from "./../utils/helpers";
+
 export default {
   name: "searchbar",
+  data() {
+    return {
+      ip: "",
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        let ip = this.ip.trim();
+        if (!this.ip.length) {
+          Toast.fire({
+            icon: "warning",
+            title: "Please enter IP adress.",
+          });
+          return;
+        }
+        const pattern =
+          /((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/g;
+        if (!pattern.test(ip)) {
+          Toast.fire({
+            icon: "warning",
+            title: "Please enter valid IP address.",
+          });
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "warning",
+          title: "An error occurred,please try again later",
+        });
+      }
+    },
+  },
 };
 </script>
