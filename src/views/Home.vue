af184d9c0be1ae237bb8e0133c7d9a64b6a3bee4
ChiaHui <bornonefreekiss@alum.ccu.edu.tw>
2021-09-22 20:02:56 +0800

    feat: fech ip geo api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,34 @@
           IP Address Tracker
         </div>
         <div>
-          <Searchbar />
+          <div class="search-bar w-full sm:w-2/5 flex sm:mx-auto">
+            <form class="w-full" @submit.stop.prevent="handleSubmit">
+              <input
+                type="text"
+                v-model="ipAddress"
+                class="
+                  h-16
+                  w-10/12
+                  xl:w-9/10
+                  text-gary-deufult text-md
+                  placeholder-gray placeholder-opacity-75
+                  rounded-l-xl
+                  py-1
+                  px-2
+                  lg:px-5
+                "
+                maxlength="20"
+                placeholder="Search for any IP address."
+              />
+              <button
+                class="h-16 bg-black rounded-r-xl w-2/12 xl:w-1/10 py-1"
+                type="submit"
+                :disabled="isProcessing"
+              >
+                <span class="text-white font-bold btn-icon">></span>
+              </button>
+            </form>
+          </div>
 
           <h1 class="text-white text-2xl font-medium text-center my-8">
             Your IP address is <span>{{ this.ip }}</span
@@ -54,18 +81,30 @@
 </template>
 
 <script>
-import Searchbar from "../components/Searchbar.vue";
 import { Toast } from "./../utils/helpers";
 
 import axios from "axios";
 export default {
   name: "Home",
-  components: {
-    Searchbar,
-  },
+
   data() {
     return {
       ip: "-",
+      ipAddress: "",
+      ipAddressData: {
+        ip: "-",
+        isp: "-",
+        location: {
+          city: "-",
+          country: "-",
+          lat: 0,
+          lng: 0,
+          postalCode: "",
+          region: "-",
+          timezone: "00:00",
+        },
+      },
+      isProcessing: false,
     };
   },
   created() {
@@ -99,6 +138,61 @@ export default {
         icon: "warning",
         title: "Failed to copy ip,please try again later",
       });
+    },
+    async handleSubmit() {
+      try {
+        let queryIp = this.ipAddress.trim();
+        if (!this.ipAddress.length) {
+          Toast.fire({
+            icon: "warning",
+            title: "Please enter IP adress.",
+          });
+          return;
+        }
+        const pattern =
+          /((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}/g;
+        if (!pattern.test(queryIp)) {
+          Toast.fire({
+            icon: "warning",
+            title: "Please enter valid IP address.",
+          });
+          return;
+        }
+        this.isProcessing = true;
+        const response = await axios.get(`
+        https://geo.ipify.org/api/v1?apiKey=${process.env.VUE_APP_IP_KEY}&ipAddress=${queryIp}`);
+        if (response.status !== 200) {
+          throw new Error(response.message);
+        }
+        const { data } = response;
+        console.log(data);
+        const { ip, isp, location } = data;
+        const { city, country, lat, lng, postalCode, region, timezone } =
+          location;
+        this.ipAddressData = {
+          ...this.ipAddressData,
+          ip: ip,
+          isp: isp,
+          location: {
+            city: city,
+            country: country,
+            lat: lat,
+            lng: lng,
+            postalCode: postalCode,
+            region: region,
+            timezone: timezone,
+          },
+        };
+        this.$router.push({ name: "result" });
+        this.isProcessing = false;
+      } catch (error) {
+        this.isProcessing = false;
+        console.log(error);
+        Toast.fire({
+          icon: "warning",
+          title: "An error occurred,please try again later",
+        });
+      }
     },
   },
 };
